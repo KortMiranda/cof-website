610c67b6e969160bb3caeb06223f26e9d1b49f99
kortmiranda <kortneymiranda@outlook.com>
2022-05-12 16:29:00 -0500

    update favicon

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-
-// import App from './App';
-
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//       <App />
-//   </React.StrictMode>
-// );
 
 import React from "react";
 import ReactDOM from "react-dom";
-// import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-// import reportWebVitals from "./reportWebVitals";
+
 
 ReactDOM.render(
     <React.StrictMode>
@@ -27,6 +14,3 @@ ReactDOM.render(
     document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// reportWebVitals();
